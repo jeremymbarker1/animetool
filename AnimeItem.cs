9a5ce8f9da3de18822cea2ebe500344dfc1f76b8
Jeremy Barker <itsjeremybarker@gmail.com>
2023-06-03 20:35:24 -0400

    Add project files.

---
+++ AnimeItem.cs
@@ -0,0 +1,30 @@
+namespace AnimeTool
+{
+    // List item compatible with MyAnimeList
+    class AnimeItem
+    {
+        public string series_animedb_id;
+        public string series_title;
+        public string series_type;
+        public string series_episodes;
+        public string my_id;
+        public string my_watched_episodes;
+        public string my_start_date;
+        public string my_finish_date;
+        public string my_rated;
+        public string my_score;
+        public string my_storage;
+        public string my_storage_value;
+        public string my_status;
+        public string my_comments;
+        public string my_times_watched;
+        public string my_rewatch_value;
+        public string my_priority;
+        public string my_tags;
+        public string my_rewatching;
+        public string my_rewatching_ep;
+        public string my_discuss;
+        public string my_sns;
+        public string update_on_import;
+    }
+}
